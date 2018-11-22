5a5eccfaf9e12af63f2306c8ded1c5f759ad1093
JGugel <jgugel@purdue.edu>
2018-11-22 08:37:10 -0500

    Added JDBC and driver classes

--- PerfectPantryApp/src/PerfectPantryGUI.java
+++ PerfectPantryApp/src/PerfectPantryGUI.java
@@ -1,14 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.table.*;
+
+
+
 
 /**
- *
- * @author Hira Waqas
+ * This class creates the GUI for the Perfect Pantry application.
+ * @author Hira Waqas, Josh Gugel
  */
-public class PerfectPantryGUI extends javax.swing.JFrame {
+public class PerfectPantryGUI extends JFrame {
 
     /**
      * Creates new form PerfectPantryGUI
@@ -26,74 +27,74 @@ public class PerfectPantryGUI extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        inventoryTabPane = new javax.swing.JTabbedPane();
-        inventorySplitPane = new javax.swing.JSplitPane();
-        inventoryLeftPanel = new javax.swing.JPanel();
-        addInventoryButton = new javax.swing.JButton();
-        categoriesPanel = new javax.swing.JPanel();
-        fruitCheckBox = new javax.swing.JCheckBox();
-        inventoryRightPanel = new javax.swing.JPanel();
-        sortingPanel = new javax.swing.JPanel();
-        filterLabel = new javax.swing.JLabel();
-        sortingLabel = new javax.swing.JLabel();
-        sortingComboBox = new javax.swing.JComboBox<>();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        inventoryTable = new javax.swing.JTable();
+        inventoryTabPane = new JTabbedPane();
+        inventorySplitPane = new JSplitPane();
+        inventoryLeftPanel = new JPanel();
+        addInventoryButton = new JButton();
+        categoriesPanel = new JPanel();
+        fruitCheckBox = new JCheckBox();
+        inventoryRightPanel = new JPanel();
+        sortingPanel = new JPanel();
+        filterLabel = new JLabel();
+        sortingLabel = new JLabel();
+        sortingComboBox = new JComboBox<>();
+        jScrollPane1 = new JScrollPane();
+        inventoryTable = new JTable();
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setTitle("Perfect Pantry");
 
         addInventoryButton.setText("Add Inventory");
-        addInventoryButton.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        addInventoryButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
                 addInventoryButtonActionPerformed(evt);
             }
         });
 
-        categoriesPanel.setBorder(javax.swing.BorderFactory.createTitledBorder("Categories"));
+        categoriesPanel.setBorder(BorderFactory.createTitledBorder("Categories"));
 
         fruitCheckBox.setText("Fruits");
-        fruitCheckBox.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        fruitCheckBox.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
                 fruitCheckBoxActionPerformed(evt);
             }
         });
 
-        javax.swing.GroupLayout categoriesPanelLayout = new javax.swing.GroupLayout(categoriesPanel);
+        GroupLayout categoriesPanelLayout = new GroupLayout(categoriesPanel);
         categoriesPanel.setLayout(categoriesPanelLayout);
         categoriesPanelLayout.setHorizontalGroup(
-            categoriesPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            categoriesPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
             .addGroup(categoriesPanelLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(fruitCheckBox)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         categoriesPanelLayout.setVerticalGroup(
-            categoriesPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            categoriesPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
             .addGroup(categoriesPanelLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(fruitCheckBox)
                 .addContainerGap(70, Short.MAX_VALUE))
         );
 
-        javax.swing.GroupLayout inventoryLeftPanelLayout = new javax.swing.GroupLayout(inventoryLeftPanel);
+        GroupLayout inventoryLeftPanelLayout = new GroupLayout(inventoryLeftPanel);
         inventoryLeftPanel.setLayout(inventoryLeftPanelLayout);
         inventoryLeftPanelLayout.setHorizontalGroup(
-            inventoryLeftPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            inventoryLeftPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
             .addGroup(inventoryLeftPanelLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(inventoryLeftPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(categoriesPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(addInventoryButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(inventoryLeftPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING, false)
+                    .addComponent(categoriesPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(addInventoryButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         inventoryLeftPanelLayout.setVerticalGroup(
-            inventoryLeftPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            inventoryLeftPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
             .addGroup(inventoryLeftPanelLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(addInventoryButton)
                 .addGap(18, 18, 18)
-                .addComponent(categoriesPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(categoriesPanel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
                 .addContainerGap(198, Short.MAX_VALUE))
         );
 
@@ -103,38 +104,38 @@ public class PerfectPantryGUI extends javax.swing.JFrame {
 
         sortingLabel.setText("Sorting:");
 
-        sortingComboBox.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Name", "Categories" }));
-        sortingComboBox.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+        sortingComboBox.setModel(new DefaultComboBoxModel<>(new String[] { "Name", "Categories" }));
+        sortingComboBox.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
                 sortingComboBoxActionPerformed(evt);
             }
         });
 
-        javax.swing.GroupLayout sortingPanelLayout = new javax.swing.GroupLayout(sortingPanel);
+        GroupLayout sortingPanelLayout = new GroupLayout(sortingPanel);
         sortingPanel.setLayout(sortingPanelLayout);
         sortingPanelLayout.setHorizontalGroup(
-            sortingPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            sortingPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
             .addGroup(sortingPanelLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(filterLabel)
                 .addGap(35, 35, 35)
                 .addComponent(sortingLabel)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(sortingComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(sortingComboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         sortingPanelLayout.setVerticalGroup(
-            sortingPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            sortingPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
             .addGroup(sortingPanelLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(sortingPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                .addGroup(sortingPanelLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
                     .addComponent(filterLabel)
                     .addComponent(sortingLabel)
-                    .addComponent(sortingComboBox, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                    .addComponent(sortingComboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
-        inventoryTable.setModel(new javax.swing.table.DefaultTableModel(
+        inventoryTable.setModel(new DefaultTableModel(
             new Object [][] {
                 {null, null, null, null},
                 {null, null, null, null},
@@ -147,44 +148,44 @@ public class PerfectPantryGUI extends javax.swing.JFrame {
         ));
         jScrollPane1.setViewportView(inventoryTable);
 
-        javax.swing.GroupLayout inventoryRightPanelLayout = new javax.swing.GroupLayout(inventoryRightPanel);
+        GroupLayout inventoryRightPanelLayout = new GroupLayout(inventoryRightPanel);
         inventoryRightPanel.setLayout(inventoryRightPanelLayout);
         inventoryRightPanelLayout.setHorizontalGroup(
-            inventoryRightPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            inventoryRightPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
             .addGroup(inventoryRightPanelLayout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(inventoryRightPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(sortingPanel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(inventoryRightPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
+                    .addComponent(sortingPanel, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(inventoryRightPanelLayout.createSequentialGroup()
-                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 407, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jScrollPane1, GroupLayout.PREFERRED_SIZE, 407, GroupLayout.PREFERRED_SIZE)
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         inventoryRightPanelLayout.setVerticalGroup(
-            inventoryRightPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            inventoryRightPanelLayout.createParallelGroup(GroupLayout.Alignment.LEADING)
             .addGroup(inventoryRightPanelLayout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(sortingPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 298, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addComponent(sortingPanel, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, GroupLayout.PREFERRED_SIZE, 298, GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         inventorySplitPane.setRightComponent(inventoryRightPanel);
 
         inventoryTabPane.addTab("Inventory", inventorySplitPane);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        GroupLayout layout = new GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(inventoryTabPane)
                 .addContainerGap())
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            layout.createParallelGroup(GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(inventoryTabPane)
@@ -194,67 +195,34 @@ public class PerfectPantryGUI extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void addInventoryButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_addInventoryButtonActionPerformed
+    private void addInventoryButtonActionPerformed(ActionEvent evt) {//GEN-FIRST:event_addInventoryButtonActionPerformed
         // TODO add your handling code here:
         
     }//GEN-LAST:event_addInventoryButtonActionPerformed
 
-    private void sortingComboBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_sortingComboBoxActionPerformed
+    private void sortingComboBoxActionPerformed(ActionEvent evt) {//GEN-FIRST:event_sortingComboBoxActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_sortingComboBoxActionPerformed
 
-    private void fruitCheckBoxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_fruitCheckBoxActionPerformed
+    private void fruitCheckBoxActionPerformed(ActionEvent evt) {//GEN-FIRST:event_fruitCheckBoxActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_fruitCheckBoxActionPerformed
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(PerfectPantryGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(PerfectPantryGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(PerfectPantryGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(PerfectPantryGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new PerfectPantryGUI().setVisible(true);
-            }
-        });
-    }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton addInventoryButton;
-    private javax.swing.JPanel categoriesPanel;
-    private javax.swing.JLabel filterLabel;
-    private javax.swing.JCheckBox fruitCheckBox;
-    private javax.swing.JPanel inventoryLeftPanel;
-    private javax.swing.JPanel inventoryRightPanel;
-    private javax.swing.JSplitPane inventorySplitPane;
-    private javax.swing.JTabbedPane inventoryTabPane;
-    private javax.swing.JTable inventoryTable;
-    private javax.swing.JScrollPane jScrollPane1;
-    private javax.swing.JComboBox<String> sortingComboBox;
-    private javax.swing.JLabel sortingLabel;
-    private javax.swing.JPanel sortingPanel;
+    private JButton addInventoryButton;
+    private JPanel categoriesPanel;
+    private JLabel filterLabel;
+    private JCheckBox fruitCheckBox;
+    private JPanel inventoryLeftPanel;
+    private JPanel inventoryRightPanel;
+    private JSplitPane inventorySplitPane;
+    private JTabbedPane inventoryTabPane;
+    private JTable inventoryTable;
+    private JScrollPane jScrollPane1;
+    private JComboBox<String> sortingComboBox;
+    private JLabel sortingLabel;
+    private JPanel sortingPanel;
     // End of variables declaration//GEN-END:variables
 }
