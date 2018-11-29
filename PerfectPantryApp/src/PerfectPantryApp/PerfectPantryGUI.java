da6894a61e3830b3e6e5ed5b5da5eb2358aece0e
mdecaire <m.r.decaire@gmail.com>
2018-11-29 04:04:40 +0000

    uncommented the dispose

--- PerfectPantryApp/src/PerfectPantryApp/PerfectPantryGUI.java
+++ PerfectPantryApp/src/PerfectPantryApp/PerfectPantryGUI.java
@@ -142,7 +142,7 @@ public class PerfectPantryGUI extends JFrame {
             } else {
                 data = null;
             }
-                // dispose(); 
+                 dispose(); 
         }
     }
     
